86a10c14f364accb9280bb9c776cc23c0cf5db53
ecaterina <ecaterinabjr@gmail.com>
2026-04-03 00:48:19 +0300

    add map method

--- TravelJournal/src/components/Entry.tsx
+++ TravelJournal/src/components/Entry.tsx
@@ -1,35 +1,23 @@
-export default function Entry(){
+type CountryProps = {
+    img:string
+    country:string
+    location:string
+    year:string
+    description:string
+}
+
+export default function Entry({img,country,location,year,description}:CountryProps){
     return(
         <>
-            <main>
         <article className="entry-article">
-            <img src="/src/1.jpg" alt="japan"></img>
+            <img src={img} alt="japan"></img>
             <div className="article-content">
-                <h5 className="country">JAPAN</h5>
-                <h1>Mount Fuji</h1>
-                <h5>12 Jan, 2021 - 24 Jan, 2021</h5>
-                <p>Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. </p>
+                <h5 className="country">{country}</h5>
+                <h1>{location}</h1>
+                <h5>{year}</h5>
+                <p>{description}</p>
             </div>
         </article>
-                <article className="entry-article">
-                <img src="/src/2.jpg" alt="australia"></img>
-                <div className="article-content">
-                    <h5 className="country">AUSTRALIA</h5>
-                    <h1>Sydney Opera House</h1>
-                    <h5>12 Jan, 2021 - 24 Jan, 2021</h5>
-                    <p>Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. </p>
-                </div>
-            </article>
-            <article className="entry-article">
-                <img src="/src/3.jpg" alt="japan"></img>
-                <div className="article-content">
-                    <h5 className="country">NORWAY</h5>
-                    <h1>Geirangerfjord</h1>
-                    <h5>12 Jan, 2021 - 24 Jan, 2021</h5>
-                    <p>Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. </p>
-                </div>
-            </article>
-            </main>
         </>
     )
 }
